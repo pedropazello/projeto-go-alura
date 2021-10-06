f1ffa03e10b6e81a5dd5f224e3233b20905026ee
Pedro Pazello <email@pedropazello.com>
2021-10-05 23:20:37 -0300

    finaliza aula 2

--- src/hello/hello.go
+++ src/hello/hello.go
@@ -3,5 +3,28 @@ package main
 import "fmt"
 
 func main() {
-  fmt.Println("Olá Mundo")
+  nome := "Pedro"
+  versao := 1.1
+
+  fmt.Println("Olá, sr.", nome)
+  fmt.Println("Este programa está na versão", versao)
+
+  fmt.Println("1- Iniciar Monitoramente")
+  fmt.Println("2- Exibir Logs")
+  fmt.Println("0- Sair do Programa")
+
+  var comando int
+  fmt.Scan(&comando)
+
+  fmt.Println("O comando escolhido foi", comando)
+
+  if comando == 1 {
+    fmt.Println("Monitorando...")
+  } else if comando == 2 {
+    fmt.Println("Exibindo Logs...")
+  } else if comando == 0 {
+    fmt.Println("Saindo do programa")
+  } else {
+    fmt.Println("Não conheço este comando")
+  }
 }

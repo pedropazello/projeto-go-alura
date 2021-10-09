a73ab5686d8becd79e769bcc821c6b226339b0f3
Pedro Pazello <email@pedropazello.com>
2021-10-09 15:31:31 -0300

    finaliza aula final do curso

--- src/hello/hello.go
+++ src/hello/hello.go
@@ -8,6 +8,8 @@ import (
   "bufio"
   "io"
   "strings"
+  "strconv"
+  "io/ioutil"
 )
 
 const monitoramentos = 3
@@ -25,6 +27,7 @@ func main() {
         iniciarMonitoramento()
       case 2:
         fmt.Println("Exibindo Logs...")
+        imprimeLogs()
       case 0:
         fmt.Println("Saindo do programa")
         os.Exit(0)
@@ -85,8 +88,10 @@ func testaSite(site string) {
 
   if resp.StatusCode == 200 {
     fmt.Println("Site:", site, "foi carregado com sucesso")
+    registraLogs(site, true)
   } else {
     fmt.Println("Site:", site, "está com problemas. Status Code:", resp.StatusCode)
+    registraLogs(site, false)
   }
 }
 
@@ -117,4 +122,25 @@ func leSitesArquivo() []string {
 
   arquivo.Close()
   return sites
+}
+
+func registraLogs(site string, carregado bool) {
+  arquivo, err := os.OpenFile("log.txt", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+
+  if err != nil {
+    fmt.Println(err)
+  }
+
+  arquivo.WriteString(time.Now().Format("02/01/2006 15:04:05") + " - " + site + " - online: " + strconv.FormatBool(carregado) + "\n")
+  arquivo.Close()
+}
+
+func imprimeLogs() {
+  arquivo, err := ioutil.ReadFile("log.txt")
+
+  if err != nil {
+    fmt.Println(err)
+  }
+
+  fmt.Println(string(arquivo))
 }
